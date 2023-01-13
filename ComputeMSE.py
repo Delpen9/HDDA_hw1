d6eb20371ac9e3e1bff6011eb2cc649922a03890
Delpen9 <iantdover@gmail.com>
2023-01-13 16:23:30 -0600

    Update ComputeMSE.py

--- ComputeMSE.py
+++ ComputeMSE.py
@@ -1,22 +1,17 @@
 import numpy as np
-from scipy.interpolate import UnivariateSpline
 
-def compute_mse(x, y, spline):
+def compute_mse(
+    x : np.ndarray,
+    y : np.ndarray,
+    spline : callable
+) -> float:
     """
-    Compute the mean squared error of a spline
-    x: array of x-coordinates of data points
-    y: array of y-coordinates of data points
-    spline: spline object
+    compute_mse():
+        Compute the mean squared error of a spline
+        x: array of x-coordinates of data points
+        y: array of y-coordinates of data points
+        spline: spline object
     """
     y_pred = spline(x)
     mse = np.mean((y - y_pred)**2)
     return mse
-
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([1, 2, 3, 4, 5])
-
-# Create the spline
-spline = UnivariateSpline(x, y, s=0.1)
-
-mse = compute_mse(x, y, spline)
-print("Mean Squared Error: ", mse)
