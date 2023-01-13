255c2310eda5c317d0997a2cb179bf35c2b9dfae
Delpen9 <iantdover@gmail.com>
2023-01-13 16:14:57 -0600

    Update PlotMeanFunction.py

--- PlotMeanFunction.py
+++ PlotMeanFunction.py
@@ -2,17 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import UnivariateSpline
 
-# Generate some data
-x = np.linspace(0, 10, num=100)
-y = np.sin(x) + np.random.normal(0, 0.2, size=100)
+def plot_mean_function(
+  x : np.ndarray,
+  y : np.ndarray,
+  spline : Object
+) -> None:
+  '''
+  plot_mean_function():
+    
+  '''
+  # Plot the sample average signal
+  plt.plot(x, y, label = 'Sample Average Signal')
 
-# Create the spline
-spline = UnivariateSpline(x, y, s=0.1)
-
-# Plot the sample average signal
-plt.plot(x, y, label='Sample Average Signal')
-
-# Plot the estimated mean function
-plt.plot(x, spline(x), label='Estimated Mean Function', color='red')
-plt.legend()
-plt.show()
+  # Plot the estimated mean function
+  plt.plot(x, spline(x), label = 'Estimated Mean Function', color = 'red')
+  plt.legend()
+  plt.show()
