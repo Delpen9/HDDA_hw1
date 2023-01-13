e58a53d71ae6c62cd7f803ee5af35d49c8522b07
Delpen9 <iantdover@gmail.com>
2023-01-13 16:50:19 -0600

    Update BSpline.py

--- BSpline.py
+++ BSpline.py
@@ -13,6 +13,13 @@ def b_spline(
         y: array of y-coordinates of data points
         degree: degree of the B-spline
     '''
-    knots = np.arange(len(x)).astype(float)
+    knot_count = degree - 2
+    knot_step = (max(x) - min(x)) / knot_count
+    knots = np.concatenate((
+        min(x),
+        np.arange(min(X) + knot_step, max(x), knot_step).astype(float),
+        max(x)
+    ))
+    
     spline = BSpline(knots, y, degree)
     return spline
