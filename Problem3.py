99e7c879435d01013760161d6129b9d390adfa5c
Delpen9 <iantdover@gmail.com>
2023-01-13 16:29:27 -0600

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,10 @@
+from BSpline import b_spline
+from CubicSpline import cubic_spline
+from KernelRegression import kernel_regression
+from SmoothingSpline import smoothing_spline
+
+from ComputeMSE import compute_mse
+from PlotMeanFunction import plot_mean_function
+
+if __name__ == '__main__':
+  print(None)
